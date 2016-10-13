a07d78aaa3d4a34218dcf5aebdfa3aa0cb0f06d4
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-10-13 18:37:20 -0500

    missed a change

--- dist/main.js
+++ dist/main.js
@@ -9,7 +9,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:5279')
+  win.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   win.webContents.openDevTools()
